1042a412cdd9d8304c756d9c637a785b3d1839f0
veeoid <vc7410@rit.edu>
2025-11-28 12:28:49 -0600

    Basic ChatApp structure

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const {Server} = require('socket.io')
+const path = require('path')
+
+PORT = 9000;
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const io = new Server();
+io.attach(httpServer)
+
+io.on('connection', (socket)=>{
+    console.log(`New User Connected: ${socket}`);
+})
+
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/', (req, res)  => {
+    return res.sendFile('/public/index.html')
+})
+
+
+httpServer.listen(PORT, () => {console.log(`listening at PORT: ${PORT}`)})
+
